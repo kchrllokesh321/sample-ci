fc108292d23a043fe14ba0053c60ed45ee5a1389
kchrllokesh321 <152631334+kchrllokesh321@users.noreply.github.com>
2025-09-26 10:29:17 +0530

    Create app.py

---
+++ app/app.py
@@ -0,0 +1,15 @@
+# app/app.py
+import http.server
+import socketserver
+
+PORT = 8000
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Hello from CI-built Docker image!")
+
+if __name__ == "__main__":
+    with socketserver.TCPServer(("", PORT), Handler) as httpd:
+        httpd.serve_forever()
